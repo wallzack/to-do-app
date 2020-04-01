bbd1115b1fd2044a3d38b9567a3dbe3603101926
wallzack <vulcheek@gmail.com>
2020-04-01 19:32:55 +0200

    22.4 added socket.io emit and broadcast

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    console.log('New task added ' + task);
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (taskIndex) => {
+    console.log('Task with index ' + taskIndex + ' removed');
+    tasks.splice(taskIndex, 1);
+    socket.broadcast.emit('removeTask');
+  });
 });
 
 app.use((req, res) => {
