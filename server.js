287571861e698d0cc29a649f2a0eb98e3ad395a6
wallzack <vulcheek@gmail.com>
2020-04-03 11:17:51 +0200

    22.4 finished internal tests. Task ready for review

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('removeTask', id);
   });
 });
-//test3
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Page not found...' });
 });
